1d006a3acf0dc334af659ae8a9873fc134cb9821
Kfmoss <berlin1980e@gmail.com>
2025-02-27 21:52:37 +0100

    working on

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ let editor = document.querySelector("#editor");
 let runCodeButton = document.querySelector("#runCode");
 let resetCodeButton = document.querySelector('.editor_reset');
 
-const myeditor= ace.edit(editor, {
+const myeditor= ace.edit("editor", {
     theme: "ace/theme/cobalt",
     mode: "ace/mode/javascript",
-})
+});
 
 
 runCodeButton.addEventListener('click', ()=> {
     const code = myeditor.getValue();
     const result = eval(code);
+    console.log(result)
 
 
 });
