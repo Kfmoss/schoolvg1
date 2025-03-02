4fe5e2d7f19c913280fb72d2e7269eb88aaa7777
Kfmoss <berlin1980e@gmail.com>
2025-03-02 10:36:10 +0100

    variabler ready

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ eksempel1.addEventListener("click", function () {
     alert("Dette er eksempel 1 Callback");
 
 });
+
+
+const PI = 3.14;
+PI = 3.1415; // Feil! Kan ikke endres
+
+const user = { name: "Ola" };
+user.name = "Kari"; // Dette er LOV! (objekter kan endres, men ikke re-deklareres)
+
+
+
