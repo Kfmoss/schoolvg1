e22f54e0b4a3d60db62b9f9531bead5235e0572f
Kfmoss <berlin1980e@gmail.com>
2025-03-08 10:39:49 +0100

    for loops

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 let eksempel1 = document.querySelector("#myCallbackButton1")
+const tittle = "the problem"
+
+
 
 eksempel1.addEventListener("click", function () {
     alert("Dette er eksempel 1 Callback");
@@ -7,11 +10,28 @@ eksempel1.addEventListener("click", function () {
 });
 
 
-function leggSammen(a, b) {
-    return a + b;
-}
 
-console.log(leggSammen(5, 10)); // 15
+let tall = [2, 4, 6, 8];
 
+tall.forEach(function (verdi) {
+    console.log(verdi); // Skriver ut: 2, 4, 6, 8
+});
+
+
+let colors=["white", "blue", "orange", "green"];
+
+colors.forEach(verdi => console.log(verdi));
+
+
+
+let personer = [
+    { navn: "Ole", alder: 25 },
+    { navn: "Lise", alder: 30 },
+    { navn: "Kari", alder: 28 }
+];
+
+personer.forEach(person => {
+    console.log(`${person.navn} er ${person.alder} år.`);
+});
 
 
