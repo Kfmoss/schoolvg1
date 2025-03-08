8825d605a0a8ae133f428b14f56006091ae995d2
Kfmoss <berlin1980e@gmail.com>
2025-03-08 12:05:18 +0100

    for loops

--- index.js
+++ index.js
@@ -11,27 +11,8 @@ eksempel1.addEventListener("click", function () {
 
 
 
-let tall = [2, 4, 6, 8];
-
-tall.forEach(function (verdi) {
-    console.log(verdi); // Skriver ut: 2, 4, 6, 8
-});
-
-
-let colors=["white", "blue", "orange", "green"];
-
-colors.forEach(verdi => console.log(verdi));
 
 
 
-let personer = [
-    { navn: "Ole", alder: 25 },
-    { navn: "Lise", alder: 30 },
-    { navn: "Kari", alder: 28 }
-];
-
-personer.forEach(person => {
-    console.log(`${person.navn} er ${person.alder} år.`);
-});
 
 
