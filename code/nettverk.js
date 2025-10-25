a569de99a958d75cf871351e350e50a012c2e215
Kfmoss <berlin1980e@gmail.com>
2025-10-25 13:12:49 +0200

    LAN

--- code/nettverk.js
+++ code/nettverk.js
@@ -1,5 +1,49 @@
 
+
+
+
 let btn = document.getElementById('toggle');
 btn.addEventListener('click', () => {
 document.body.classList.toggle('light-mode');
 });
+
+
+let isReading = false;
+let listenersAdded = false;
+
+
+
+function main_speech() {
+    if (listenersAdded) return;
+    listenersAdded = true;
+    document.querySelectorAll('i').forEach(x =>{
+        x.addEventListener('click', function(){
+            if(isReading){
+                speechSynthesis.cancel();
+                isReading = false;
+                return;
+            }
+            const container = x.closest('section');
+            if(container){
+                isReading = true;
+                const selector =container.id;
+                console.log(selector);
+                const speechElem = document.querySelector('#'+selector);
+                let text = speechElem.textContent.trim();
+                const lyd =  new SpeechSynthesisUtterance(text);
+                lyd.lang = "nb-NO";
+                lyd.onend = function(){
+                    isReading = false();
+                };
+                speechSynthesis.speak(lyd);
+
+             }
+        })
+    })
+    
+    
+    
+
+
+
+};
